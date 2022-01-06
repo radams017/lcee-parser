2f653fb02da5d9ef595e76d4ae297a6c1147f4a3
Ryan Adams <radams@li-fvfgl0ppq6lx-macbookair.attlocal.net>
2022-01-05 20:42:22 -0600

    formatted

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const  EmlParser = require('eml-parser');
-const  fs = require('fs');
-const { isRegExp } = require('util');
+const EmlParser = require("eml-parser");
+const fs = require("fs");
+const { isRegExp } = require("util");
 
 // Search through the HTML string and return the LCTG values. Probably should refactor this into more than one function.
 const parseHTMLString = (str) => {
-    let html = str;
-    let srcUrls = html.match(/src\s*=\s*"(.+?)"/g);
-    let lctg = html.match(/lctg=\S{26}/g);
-    if(!lctg){
-        console.log('LCTG Parameter Not Found\n\nCheck Outputted HTML File for potential redirect links')
-        return
-    } else {
-    let arr = []
+  let html = str;
+  let srcUrls = html.match(/src\s*=\s*"(.+?)"/g);
+  let lctg = html.match(/lctg=\S{26}/g);
+  if (!lctg) {
+    console.log(
+      "LCTG Parameter Not Found\n\nCheck Outputted HTML File for potential redirect links"
+    );
+    return;
+  } else {
+    let arr = [];
     for (urls of srcUrls) {
-        // this needs to be refactored -- I am basically trying to remove the beginning and end quotes/src stuff but can't figure out how to chain it. This is also pretty hardcoded and relies on the URLS being the same format
-        let urlFrontTrim = urls.substring(5);
-        let finalUrls = urlFrontTrim.substring(0, urlFrontTrim.length - 1);
-        arr.push(finalUrls);
+      // this needs to be refactored -- I am basically trying to remove the beginning and end quotes/src stuff but can't figure out how to chain it. This is also pretty hardcoded and relies on the URLS being the same format
+      let urlFrontTrim = urls.substring(5);
+      let finalUrls = urlFrontTrim.substring(0, urlFrontTrim.length - 1);
+      arr.push(finalUrls);
     }
-    let lctgArr = arr.filter(url => url.match(/.*lctg=([^&|\n|\t\s]+)/));
+    let lctgArr = arr.filter((url) => url.match(/.*lctg=([^&|\n|\t\s]+)/));
     console.log(lctgArr);
- }
-}
+  }
+};
 
 // Decode from Quoted-Printable
 new EmlParser(fs.createReadStream(process.argv[2]))
-.getEmailBodyHtml()
-.then(htmlString => {
-    fs.writeFileSync('decoded_email.html',htmlString)
-    parseHTMLString(htmlString)
-})
-.catch(err => {
+  .getEmailBodyHtml()
+  .then((htmlString) => {
+    fs.writeFileSync("decoded_email.html", htmlString);
+    parseHTMLString(htmlString);
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
