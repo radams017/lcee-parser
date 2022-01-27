e0a626a8fcd807828bfbda020edb4eb3e2f84560
Ryan Adams <radams@li-fvfgl0ppq6lx-macbookair.attlocal.net>
2022-01-27 11:15:02 -0600

    added congruency check for all parameter values

--- cheerio.js
+++ cheerio.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const request = require("request");
 const rp = require("request-promise");
 const Promise = require("bluebird");
+const inquirer = require("inquirer");
 
 function parseHTML(str) {
   const $ = cheerio.load(str);
@@ -16,15 +17,10 @@ function parseHTML(str) {
   });
   let result = links.map((a) => a.href);
   let hrefs = result.filter((url) => url.match(/http[s]?/));
-  return Promise.map(hrefs, getRedirectedURL);
+  return Promise.map(hrefs, getRedirectedURL); // promise.map('array of promises') => resolve to a new array with answer to each promise
 }
 
 function retrieveURL(response, body) {
-  //   console.log("Init URL:", url);
-  //   console.log("statusCode:", response && response.statusCode);
-  // console.log(response)
-  // console.log(JSON.stringify(body))
-  // let redURL = response.request.uri.href;
   let redURL = response.request.uri.href;
   // let lctgRedirectURL = lctgRedirect.match(/lctg?/)
   //   console.log(redURL, "\n");
@@ -43,21 +39,56 @@ function getRedirectedURL(url) {
   return rp(options).then(retrieveURL);
 }
 
-// promise.map('array of promises') => resolve to a new array with answer to each promise
+function checkCongruency(arr) {
+  return arr.every((c) => c === arr[0])
+}
+
+function getFinalUrls(ans) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "parameter",
+        message: "What is the name of the LCEE URL Parameter?",
+      },
+    ])
+    .then((response) => {
+      let param = response.parameter;
+      let paramExp = `${param}=(.{1,}?)&`;
+      const paramReg = new RegExp(`${param}=(.{1,}?)&`, "i");
+      let matchedUrls = ans.filter((a) => a.match(param));
+      // console.log(matchedUrls);
+
+      let paramValues = matchedUrls.map((b) => {
+        let match = paramReg.exec(b);
+        return match[1];
+      });
+      console.log(matchedUrls)
+      console.log(paramValues);
+      console.log("Values Are Congruent: ", checkCongruency(paramValues));
+
+      // Keeping the below for now in case we want only unique urls to check -- might want to see every URL though.
+      // let uniq = [...new Set(matchedUrls)];
+    });
+}
 
 // getRedirectedURL('http://enews.newsletters.ocregister.com/q/FK8ps9rHfGQH0XS2aeoNHATJ4Lw9ntACpd2IZcOJY2hyaXN0ZW5sb2Jvc2NvQGdtYWlsLmNvbcOI2waCAOzNcrYk8oiIU4on290HgQ', function(err, response) {
 //     let reURL = response.request.uri.href;
 //     console.log(reURL)
 // })
 
-new EmlParser(fs.createReadStream(process.argv[2]))
-  .getEmailBodyHtml()
-  .then((htmlString) => {
-    fs.writeFileSync("decoded_email.html", htmlString);
-    return parseHTML(htmlString).then((answers) => {
-      console.log(answers);
+function init() {
+  new EmlParser(fs.createReadStream(process.argv[2]))
+    .getEmailBodyHtml()
+    .then((htmlString) => {
+      fs.writeFileSync("decoded_email.html", htmlString);
+      return parseHTML(htmlString).then((answers) => {
+        getFinalUrls(answers);
+      });
+    })
+    .catch((err) => {
+      console.log("Unhandled Error: ", err);
     });
-  })
-  .catch((err) => {
-      console.log("Unhandled Error: ", err)
-  });
+}
+
+init();
